8ad82f42f972eb16115cd6c08d82b4e10a98ca22
ItsFlicker <1812496278@qq.com>
2022-06-24 21:02:51 +0800

    [2.0.0-PRE-16] Fix building

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     id("org.gradle.java")
     id("org.gradle.maven-publish")
@@ -20,6 +22,10 @@ subprojects {
     tasks.withType<JavaCompile> {
         options.encoding = "UTF-8"
     }
+    tasks.withType<KotlinCompile> { kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs = listOf("-Xjvm-default=all")
+    }  }
     configure<JavaPluginConvention> {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8

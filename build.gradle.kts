ef32345a42fc383f50d4bb8c679e1888799b2004
ItsFlicker <1812496278@qq.com>
2022-08-07 23:22:17 +0800

    [2.0.0-PRE-24] Fix build?

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("org.gradle.java")
     id("org.gradle.maven-publish")
-    id("org.jetbrains.kotlin.jvm") version "1.6.21" apply false
+    id("org.jetbrains.kotlin.jvm") version "1.5.10" apply false
 }
 
 subprojects {
